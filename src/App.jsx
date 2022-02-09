867ce9c91c98356ec4d08724b2444d922016e40d
haseeb-iqbal <a.haseeb19@gmail.com>
2022-02-09 18:05:50 +0000

    Created product and productList components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import About from "./components/about/about";
 import Intro from "./components/intro/intro";
+import ProductList from "./components/productList/productList";
 
 const App = () => {
   return (
     <div>
       <Intro />
       <About />
+      <ProductList />
     </div>
   );
 };
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/10980701/pexels-photo-10980701.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/7210061/pexels-photo-7210061.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "http://lama.dev",
+  },
+];

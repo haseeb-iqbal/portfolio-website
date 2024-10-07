a692e93a1838b74bc1515529f49aca6c369a8ecf
haseeb-iqbal <a.haseeb19@gmail.com>
2024-10-07 13:35:47 +0500

    Added form validation and setup sending email

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -11,25 +11,73 @@ const Contact = () => {
   const darkMode = theme.state.darkMode;
 
   const [done, setDone] = useState(false);
+  const [sending, setSending] = useState(false);
+  const [sendingError, setSendingError] = useState(false);
+  const [formData, setFormData] = useState({
+    sender_name: "",
+    sender_email: "",
+    message: "",
+  });
+
+  const [validationErrors, setValidationErrors] = useState({});
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   const handleSubmit = (e) => {
-    setDone(false);
     e.preventDefault();
-    emailjs
-      .sendForm(
-        "service_z89eu6c",
-        "template_hd47lyk",
-        formRef.current,
-        "user_jBisT7Wa029MPwFdBLCFs"
-      )
-      .then(
-        (result) => {
-          setDone(true);
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+
+    const newErrors = validateForm(formData);
+    setValidationErrors(newErrors);
+    if (Object.keys(newErrors).length === 0) {
+      setDone(false);
+      setSending(true);
+      setSendingError(false);
+      emailjs
+        .sendForm(
+          "service_64cnrwy",
+          "template_wdlte5q",
+          formRef.current,
+          "-os34IA9CTHn9QjSs"
+        )
+        .then(
+          (result) => {
+            setDone(true);
+            console.log(result.text);
+            setSending(false);
+          },
+          (error) => {
+            console.log(error.text);
+            setSending(false);
+            setSendingError(true);
+          }
+        );
+    }
+  };
+
+  const validateForm = (data) => {
+    const errors = {};
+
+    if (!data.sender_name.trim()) {
+      errors.sender_name = "Your name is required";
+    }
+
+    if (!data.sender_email.trim()) {
+      errors.sender_email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(data.sender_email)) {
+      errors.sender_email = "Email is invalid";
+    }
+
+    if (!data.message.trim()) {
+      errors.message = "A message is required";
+    }
+
+    return errors;
   };
 
   const formRef = useRef();
@@ -61,46 +109,59 @@ const Contact = () => {
             and I will respond at my earliest convenience. I am also open for
             any general inquiries. Looking forward to speaking to you!
           </p>
-          <form ref={formRef} onSubmit={handleSubmit}>
-            <input
-              type="text"
-              style={{
-                backgroundColor: darkMode && "#333",
-                color: darkMode && "white",
-              }}
-              placeholder="Name"
-              name="from_name"
-            />
-            <input
-              type="text"
-              style={{
-                backgroundColor: darkMode && "#333",
-                color: darkMode && "white",
-              }}
-              placeholder="Subject"
-              name="user_subject"
-            />
-            <input
-              type="text"
-              style={{
-                backgroundColor: darkMode && "#333",
-                color: darkMode && "white",
-              }}
-              placeholder="Email"
-              name="user_email"
-            />
-            <textarea
-              rows="5"
-              style={{
-                backgroundColor: darkMode && "#333",
-                color: darkMode && "white",
-              }}
-              placeholder="Message"
-              name="message"
-            />
-            <button>Submit</button>
-          </form>
-          {done && "Thank you for submitting..."}
+          {done ? (
+            "Thank you for your message! I will be in contact shortly"
+          ) : (
+            <form ref={formRef} onSubmit={handleSubmit}>
+              {validationErrors.sender_name && (
+                <p className="error-message">{validationErrors.sender_name}</p>
+              )}
+              <input
+                type="text"
+                style={{
+                  backgroundColor: darkMode && "#333",
+                  color: darkMode && "white",
+                }}
+                placeholder="Name"
+                name="sender_name"
+                value={formData.sender_name}
+                onChange={handleFormChange}
+              />
+              {validationErrors.sender_email && (
+                <p className="error-message">{validationErrors.sender_email}</p>
+              )}
+              <input
+                type="text"
+                style={{
+                  backgroundColor: darkMode && "#333",
+                  color: darkMode && "white",
+                }}
+                placeholder="Email"
+                name="sender_email"
+                value={formData.sender_email}
+                onChange={handleFormChange}
+              />
+              {validationErrors.message && (
+                <p className="error-message">{validationErrors.message}</p>
+              )}
+              <textarea
+                rows="5"
+                style={{
+                  backgroundColor: darkMode && "#333",
+                  color: darkMode && "white",
+                }}
+                placeholder="Message"
+                name="message"
+                value={formData.message}
+                onChange={handleFormChange}
+              />
+
+              <button disabled={sending}>Submit</button>
+            </form>
+          )}
+
+          {sendingError &&
+            "Sorry but this doesn't seem to be working at the moment. Please try again later or contact me directly on my email address"}
         </div>
       </div>
     </div>

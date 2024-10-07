6f81932970f4e126329040f50e3d59efcb6b79ea
haseeb-iqbal <a.haseeb19@gmail.com>
2024-10-07 13:57:53 +0500

    Added additional links to Github and Upwork

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,8 +1,10 @@
 import React, { useContext, useRef, useState } from "react";
 import "./contact.css";
-import Phone from "../../img/phone.png";
-import Email from "../../img/email.png";
-import Address from "../../img/address.png";
+import EmailIcon from "../../img/email.png";
+import AddressIcon from "../../img/address.png";
+import GithubIcon from "../../img/githubIcon.svg";
+import UpworkIcon from "../../img/upworkIcon.png";
+
 import emailjs from "@emailjs/browser";
 import { ThemeContext } from "../../context";
 
@@ -88,18 +90,29 @@ const Contact = () => {
         <div className="c-left">
           <h1 className="c-title">Let's discuss your project</h1>
           <div className="c-info">
-            {/* <div className="c-info-item">
-              <img src={Phone} alt="" className="c-icon" />
-              +1 1234 556 75
-            </div> */}
             <div className="c-info-item">
-              <img src={Email} alt="" className="c-icon" />
+              <img src={EmailIcon} alt="" className="c-icon" />
               a.haseeb19@gmail.com
             </div>
-            {/* <div className="c-info-item">
-              <img src={Address} alt="" className="c-icon" />
-              +1 1234 556 75
-            </div> */}
+            <div className="c-info-item">
+              <img src={AddressIcon} alt="" className="c-icon" />
+              United Kingdom
+            </div>
+            <div className="c-info-item">
+              <img src={GithubIcon} alt="" className="c-icon" />
+              <a href="https://github.com/haseeb-iqbal" target="_blank">
+                Github
+              </a>
+            </div>
+            <div className="c-info-item">
+              <img src={UpworkIcon} alt="" className="c-icon" />
+              <a
+                href="https://www.upwork.com/freelancers/~015e70537fc3dfc00a"
+                target="_blank"
+              >
+                Upwork
+              </a>
+            </div>
           </div>
         </div>
 
